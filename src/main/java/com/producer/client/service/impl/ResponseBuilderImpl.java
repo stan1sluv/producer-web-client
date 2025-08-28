98d2a607d5341c786b65d36172659a6aadf3e35b
Stan1sluv <stanislavsenkin2112@gmail.com>
2025-08-28 19:40:03 +0300

    Change import position

--- src/main/java/com/producer/client/service/impl/ResponseBuilderImpl.java
+++ src/main/java/com/producer/client/service/impl/ResponseBuilderImpl.java
@@ -3,11 +3,10 @@ package com.producer.client.service.impl;
 import com.producer.client.api.dto.ResponseDTO;
 import com.producer.client.service.ResponseBuilder;
 
-import static com.producer.client.constants.ResponseStatus.*;
-
 import org.springframework.stereotype.Component;
 
 import static com.producer.client.constants.ResponseMessage.*;
+import static com.producer.client.constants.ResponseStatus.*;
 
 @Component
 public class ResponseBuilderImpl implements ResponseBuilder {
